ae3dc27d9a462df91ecf22fea79fddafd5c72ac2
Steve Leonard <leonarduk@users.noreply.github.com>
2026-03-18 14:36:54 +0000

    feat: add MusicXML export layer
    Closes #262

--- backend/music/__init__.py
+++ backend/music/__init__.py
@@ -6,6 +6,13 @@ from .notation_policy import (
     NotationPolicy,
     V1_NOTATION_POLICY,
 )
+from .musicxml_export import (
+    MusicXMLExportError,
+    score_model_to_music21_score,
+    score_model_to_musicxml_bytes,
+    score_model_to_musicxml_string,
+    write_score_model_musicxml,
+)
 from .score_model import (
     LyricSyllabic,
     Measure,
@@ -18,6 +25,11 @@ from .score_model import (
 )
 
 __all__ = [
+    "MusicXMLExportError",
+    "score_model_to_music21_score",
+    "score_model_to_musicxml_bytes",
+    "score_model_to_musicxml_string",
+    "write_score_model_musicxml",
     "LyricSyllabic",
     "Measure",
     "NoteScoreEvent",
